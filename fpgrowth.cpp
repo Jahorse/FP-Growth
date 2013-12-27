d864b94c052e61ecb3a9feec864ab0fac3081b26
Jahorse <jahorse@gmail.com>
2013-12-27 16:27:16 -0600

    Got the header table being created and sorted

--- fpgrowth.cpp
+++ fpgrowth.cpp
@@ -3,14 +3,12 @@
 #include <string>
 #include <vector>
 #include <cstdlib>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
-// 2D array for header table
-//int[][] createHeaderTable(){
-
-//}
-
+// debug
 void printVector(vector<vector<int> > data)
 {
 	for (int i=0; i < data.size(); i++) {
@@ -22,6 +20,45 @@ void printVector(vector<vector<int> > data)
 	}
 }
 
+// debug
+void printMap(map<int,int> data)
+{
+	map<int,int>::iterator it;
+
+	// show content:
+	for (map<int,int>::iterator it=data.begin(); it != data.end(); ++it)
+	{
+		cout << it->first << " => " << it->second << '\n';
+	}
+}
+
+// debug
+void printSortedItems(vector<pair<int,int> > items)
+{
+	for (int i=0; i < items.size(); i++)
+	{
+		cout << items.at(i).first << " -> " << items.at(i).second << endl;
+	}
+}
+
+bool sortFunction(pair<int,int> i, pair<int,int> j)
+{
+	return (i.second >= j.second);
+}
+
+vector<pair<int,int> > sortItems(map<int, int> items)
+{
+	vector<pair<int,int> > sortedItems;
+	
+	for (map<int,int>::iterator it = items.begin(); it != items.end(); ++it)
+	{
+		sortedItems.push_back(*it);
+	}
+	sort(sortedItems.begin(), sortedItems.end(), sortFunction);
+	
+	return sortedItems;
+}
+
 main()
 {
 	string line;
@@ -29,6 +66,7 @@ main()
 	int arraySize;
 	ifstream file ("test_data.txt");
 	vector<vector<int> > data;
+	map<int,int> headerTable;
 	
 	//open file
 	if (file.is_open())
@@ -68,11 +106,17 @@ main()
 			i++;
 			while (i < line.size())
 			{
+				int item;
+				
 				rawText = "";
 				while (line[i] != ' ' && i < line.size()) {
 					rawText += line[i++];
 				}
-				itemSet.push_back(atoi(rawText.c_str()));
+				item = atoi(rawText.c_str());
+				itemSet.push_back(item);
+				
+				// Add to header table
+				headerTable[item]++;
 
 				i++;
 			}
@@ -85,5 +129,10 @@ main()
 		cout << "Unable to open file";
 	}
 
-	printVector(data);
+	printVector(data); // debug
+	cout << "Map" << endl; // debug
+	printMap(headerTable); // debug
+	cout << endl; // debug
+	vector<pair<int,int> > sortedHeaderTable = sortItems(headerTable);
+	printSortedItems(sortedHeaderTable); // debug
 }
